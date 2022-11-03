b8f65d1b9d6cc12a08417f41dfc20ddfaadd98a7
andrewjtdunn <35241751+andrewjtdunn@users.noreply.github.com>
2022-11-03 16:48:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# electric-vehicle-tax-credits-Nov-2022
+ Data viz for electric vehicle tax credit article, Nov 2022
---
+++ vehicle sale share.R
@@ -0,0 +1,48 @@
+# Andrew Dunn
+# data viz to accompany Mehul Gupta's CPR "Inflation Reduction Act's EV tax credits""
+# 11.3.2022
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(ggrepel)
+
+
+setwd("~/Desktop/CPR/electric vehicle tax credits Nov 2022")
+
+full_df <- read_excel("Hybrid and Plug-In Vehicle Sales.xlsx")
+
+df <- full_df %>%
+  select(1, 6:8) %>%
+  rename("Year" = "Calendar year", "Hybrid" = "Hybrid share of all light vehicles",
+         "Plugin" = "Plug-in hybrid share of all light vehicles", 
+         "Electric" = "All-electric share of all light vehicles")
+
+ggplot(df, aes(x = Year, Hybrid)) +
+  geom_line(aes(y = Hybrid), color = "darkred") + 
+  geom_text(data = df %>% filter(Year == last(Year)), aes(label = "Hybrid", 
+                                                               x = Year + 1.3, 
+                                                               y = Hybrid) , size = 3) + 
+  geom_line(aes(y = Plugin), color = "steelblue") + 
+  geom_text(data = df %>% filter(Year == last(Year)), aes(label = "Plug-In", 
+                                                          x = Year + 1.3, 
+                                                          y = Plugin) , size = 3) +
+  geom_line(aes(y = Electric), color = "darkgreen") +
+  geom_text(data = df %>% filter(Year == last(Year)), aes(label = "Electric", 
+                                                          x = Year + 1.3, 
+                                                          y = Electric) , size = 3) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  labs(x = "Year", 
+       y = "Share of All Light Vehicles", 
+       title = "Electric Vehicle Share Has Grown Slowly",
+       subtitle = "Share of Electric Vehicle Sales relative to Hybrid and Plug-ins",
+       caption = c("\n Source: Transportation Energy Book Data Book, Edition 39 (2021) \n 
+                   Author: Andrew Dunn \n 
+                   Editor: Zai Rutter \n 
+                   Date: Nov 3, 2022"),
+       color = 'Committees') +
+  theme(plot.title = element_text(hjust = 0.5), panel.grid.minor = element_blank(), 
+        panel.grid.major = element_line(color = "azure3"),
+        panel.background = element_blank()) 
+
+
